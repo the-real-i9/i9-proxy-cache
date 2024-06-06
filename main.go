dda0cc7cb9d5780836739aeff98b9b43d0de96f3
i9 <oluwarinolasam@gmail.com>
2024-06-06 15:18:50 +0100

    proxy logic trying out

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Working!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// url key
+		originHost := "http://localhost:8080"
+		r.URL.Host = originHost
+
+		reqUrl := r.URL.String()
+
+		// the response to cache
+		proxyRes, err := http.Get(reqUrl)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(500)
+		}
+
+		proxyRes.Write(w)
+	})
+
+	http.ListenAndServe(":5000", nil)
+	fmt.Println("Server listening @ http://localhost:5000")
 }
